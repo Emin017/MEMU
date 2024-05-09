5e67cc3b78237f47936a80a0e3c17b7f38b3505e
Qiming Chu <cchuqiming@gmail.com>
2024-05-09 22:15:12 +0800

    Add zig to nix flake

--- flake.nix
+++ flake.nix
@@ -1,37 +1,50 @@
 {
-	description = "MEMU";
+  description = "MEMU";
 
-	inputs = {
-		nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-		flake-utils.url = "github:numtide/flake-utils";
-	};
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    zig-overlay.url = "github:mitchellh/zig-overlay";
+  };
 
-	outputs = { self, nixpkgs, flake-utils }@inputs:
-		flake-utils.lib.eachDefaultSystem
-		(system:
-		 let
-		 pkgs = import nixpkgs { inherit system;};
-		 deps = with pkgs; [
-		 git
-		 gnumake autoconf automake
-		 cmake ninja
-		 pkgsCross.riscv64-embedded.buildPackages.gcc
-		 ];
-		 in
-		 {
-		 legacyPackages = pkgs;
-		 devShell = pkgs.mkShell.override { stdenv = pkgs.clangStdenv; } {
-		 buildInputs = deps;
-		 RV64_TOOLCHAIN_ROOT = "${pkgs.pkgsCross.riscv64-embedded.buildPackages.gcc}";
-		 shellHook = ''
-		 export EMU_CC=$RV64_TOOLCHAIN_ROOT/bin/riscv64-none-elf-gcc
-		 export EMU_OBJCOPY=$RV64_TOOLCHAIN_ROOT/bin/riscv64-none-elf-objcopy
-		 make test-img
-		 unset EMU_CC
-		 unset EMU_OBJCOPY
-		 '';
-		 };
-		 }
-	)
-		// { inherit inputs;};
+  outputs = {
+    self,
+    zig-overlay,
+    nixpkgs,
+    flake-utils,
+  } @ inputs:
+    flake-utils.lib.eachDefaultSystem
+    (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [zig-overlay.overlays.default];
+        };
+        deps = with pkgs; [
+          git
+          gnumake
+          autoconf
+          automake
+          cmake
+          ninja
+          zigpkgs.master
+          pkgsCross.riscv64-embedded.buildPackages.gcc
+        ];
+      in {
+        legacyPackages = pkgs;
+        formatter = pkgs.alejandra;
+        devShells.default = pkgs.mkShell.override {stdenv = pkgs.clangStdenv;} {
+          buildInputs = deps;
+          RV64_TOOLCHAIN_ROOT = "${pkgs.pkgsCross.riscv64-embedded.buildPackages.gcc}";
+          shellHook = ''
+            export EMU_CC=$RV64_TOOLCHAIN_ROOT/bin/riscv64-none-elf-gcc
+            export EMU_OBJCOPY=$RV64_TOOLCHAIN_ROOT/bin/riscv64-none-elf-objcopy
+            make test-img
+            unset EMU_CC
+            unset EMU_OBJCOPY
+          '';
+        };
+      }
+    )
+    // {inherit inputs;};
 }
