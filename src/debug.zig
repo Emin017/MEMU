d991f41bb6ce1b22261c57f3d6e4dea63b147a59
Qiming Chu <cchuqiming@gmail.com>
2024-05-11 20:45:03 +0800

    Rename print buf

--- src/debug.zig
+++ src/debug.zig
@@ -11,8 +11,8 @@
 const std = @import("std");
 const print = std.debug.print;
 
-var printf_buf: [1024]u8 = undefined;
+var pbuf: [1024]u8 = undefined;
 pub fn printf(comptime format: []const u8, args: anytype) void {
-    const slice = std.fmt.bufPrint(printf_buf[0..], format, args) catch @panic("bufPrint failure");
+    const slice = std.fmt.bufPrint(pbuf[0..], format, args) catch @panic("bufPrint failure");
     print(slice);
 }
